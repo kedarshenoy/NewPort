6f1ec076c4183c032f19ae667062f1fbae1c6363
kedarshenoy <akedarshenoy@gmail.com>
2024-10-01 22:51:37 +0530

    assets added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='text-silver'>
+      <div>
+        <img className='h-8 sm:h-24' src={require('../Assets/Browser.png')} alt='Web' />
+        <img className='h-8 sm:h-24' src={require('../Assets/Discord.png')} alt='Web' />
+        <img className='h-8 sm:h-24' src={require('../Assets/Linkdin.png')} alt='Web' />
+        <img className='h-8 sm:h-24' src={require('../Assets/Mail.png')} alt='Web' />
+        <img className='h-8 sm:h-24' src={require('../Assets/Telegram.png')} alt='Web' />
+        <img className='h-8 sm:h-24' src={require('../Assets/Twiter.png')} alt='Web' />
+
+
+      </div>
+    </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Typed from 'react-typed';
 export default function Home() {
   return (
     <div className='text-white'>
@@ -10,35 +10,35 @@ export default function Home() {
       Passionate about crafting seamless web and mobile applications that connect users with technology.
       </h2>
       <div className="stacks">
-            {/* <Typed */}
-            <div
-              strings={[
-                "HTML ",
-                "CSS ",
-                "Bootstrap",
-                "Tailwind",
-                "JavaScript",
-                "React JS",
-                "Typescript",
-                "Node JS",
-                "ExpressJS",
-                "next JS",
-                "MongoDB",
-                "Mongoose",
-                "Prisma",
-                "Postgress",
-                "API",
-                "php",
-                "SQL",]}
-              typeSpeed={40}
-              backSpeed={50}
-              cursorChar='❕'
-              loop
-            >
-              <span id="element"> </span>
-            {/* </Typed> */}
+      <Typed
+                strings={[
+                  "HTML ",
+                  "CSS ",
+                  "Bootstrap",
+                  "Tailwind",
+                  "JavaScript",
+                  "React JS",
+                  "Typescript",
+                  "Node JS",
+                  "ExpressJS",
+                  "next JS",
+                  "MongoDB",
+                  "Mongoose",
+                  "Prisma",
+                  "Postgress",
+                  "API",
+                  "php",
+                  "SQL",]}
+                    typeSpeed={40}
+                    backSpeed={50}
+                    cursorChar='❕'
+                    // showCursor={false}
+                    // attr="placeholder"
+                    loop >
+                    <span id="element"> </span>
+                </Typed> 
             </div>
           </div>
-    </div>
+    // </div>
   )
 }
