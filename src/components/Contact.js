c158f5b58735f819a39c0478d1ff38c5d51badd0
kedarshenoy <akedarshenoy@gmail.com>
2024-10-02 21:48:39 +0530

    Contact Page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,17 +1,16 @@
 import React from 'react'
-
 export default function Contact() {
   return (
     <div className='text-silver'>
       <div>
         <img className='h-8 sm:h-24' src={require('../Assets/Browser.png')} alt='Web' />
-        <img className='h-8 sm:h-24' src={require('../Assets/Discord.png')} alt='Web' />
+        <a href="http://discordapp.com/users/mr_aks"
+                ><img className='h-8 sm:h-24' src={require('../Assets/Discord.png')} alt='Web' /></a>
         <img className='h-8 sm:h-24' src={require('../Assets/Linkdin.png')} alt='Web' />
-        <img className='h-8 sm:h-24' src={require('../Assets/Mail.png')} alt='Web' />
+        <a href="mailto:akedarshenoy@gmail.com" 
+                ><img className='h-8 sm:h-24' src={require('../Assets/Mail.png')} alt='Web' /></a>
         <img className='h-8 sm:h-24' src={require('../Assets/Telegram.png')} alt='Web' />
-        <img className='h-8 sm:h-24' src={require('../Assets/Twiter.png')} alt='Web' />
-
-
+        <a href="https://linkedin.com/in/argodu-kedar-shenoy" ><img className='h-8 sm:h-24' src={require('../Assets/Twiter.png')} alt='Web' /></a>
       </div>
     </div>
   )
